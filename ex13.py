f1f1419f3b176a6128f78fecf4418864de70e0bc
Chris Terrazas <chris.terrazas@gmail.com>
2020-09-14 18:18:56 -0400

    added ex13

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+fourth = raw_input("Gimme another argument > ")
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "your third variable is:", third
+print "your fourth variable is:", fourth
